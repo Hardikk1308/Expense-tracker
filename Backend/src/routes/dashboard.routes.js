1678f3c3a174f0f76471b66dcf46ee8501a2d6db
Hardikkk <hardikpawar2811@gmail.com>
2026-03-04 00:26:19 +0530

    error updated

--- Backend/src/routes/dashboard.routes.js
+++ Backend/src/routes/dashboard.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const dashboardController = require("../controllers/dashboard.controller");
+const dashboardController = require("../controllers/Dashboard.Controller");
 const authMiddleware = require("../middleware/auth.middleware");
 
 router.get("/", authMiddleware, dashboardController.getDashboard);
