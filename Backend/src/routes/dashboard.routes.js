35ce1d13bda805cbfdfd2cf06e2ca0d03812b3b1
Hardikkk <hardikpawar2811@gmail.com>
2026-03-04 00:51:10 +0530

    new

--- Backend/src/routes/dashboard.routes.js
+++ Backend/src/routes/dashboard.routes.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const router = express.Router();
-// const dashboardController = require("../controllers/dashboard.controller");
 const dashboardController = require("../controllers/Dashboard.Controller"); 
-const authMiddleware = require("../middleware/auth.middleware");
+const authMiddleware = require('../middlewares/authMiddleware');
 
 router.get("/", authMiddleware, dashboardController.getDashboard);
 
